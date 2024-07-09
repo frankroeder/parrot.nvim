f8cbaa93a80ab669927ef666dcfbcb42772222f0
Frank Roeder <fr.coding@proton.me>
2024-07-09 12:07:54 +0200

    Move Anthropic agents to general config style

--- lua/parrot/agents.lua
+++ lua/parrot/agents.lua
@@ -116,26 +116,26 @@ local pplx_chat_agents = {
 local anthropic_chat_agents = {
   {
     name = "Claude-3.5-Sonnet-Chat",
-    model = { model = "claude-3-5-sonnet-20240620", max_tokens = 4096, system = system_chat_prompt },
-    system_prompt = "",
+    model = { model = "claude-3-5-sonnet-20240620", max_tokens = 4096 },
+    system_prompt = system_chat_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Opus-Chat",
-    model = { model = "claude-3-opus-20240229", max_tokens = 4096, system = system_chat_prompt },
-    system_prompt = "",
+    model = { model = "claude-3-opus-20240229", max_tokens = 4096 },
+    system_prompt = system_chat_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Sonnet-Chat",
-    model = { model = "claude-3-sonnet-20240229", max_tokens = 4096, system = system_chat_prompt },
-    system_prompt = "",
+    model = { model = "claude-3-sonnet-20240229", max_tokens = 4096 },
+    system_prompt = system_chat_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Haiku-Chat",
-    model = { model = "claude-3-haiku-20240307", max_tokens = 4096, system = system_chat_prompt },
-    system_prompt = "",
+    model = { model = "claude-3-haiku-20240307", max_tokens = 4096 },
+    system_prompt = system_chat_prompt,
     provider = "anthropic",
   },
 }
@@ -284,22 +284,26 @@ local pplx_command_agents = {
 local anthropic_command_agents = {
   {
     name = "Claude-3.5-Sonnet",
-    model = { model = "claude-3-5-sonnet-20240620", max_tokens = 4096, system = system_code_prompt },
+    model = { model = "claude-3-5-sonnet-20240620", max_tokens = 4096 },
+    system_prompt = system_code_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Opus",
-    model = { model = "claude-3-opus-20240229", max_tokens = 4096, system = system_code_prompt },
+    model = { model = "claude-3-opus-20240229", max_tokens = 4096 },
+    system_prompt = system_code_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Sonnet",
-    model = { model = "claude-3-sonnet-20240229", max_tokens = 4096, system = system_code_prompt },
+    model = { model = "claude-3-sonnet-20240229", max_tokens = 4096 },
+    system_prompt = system_code_prompt,
     provider = "anthropic",
   },
   {
     name = "Claude-3-Haiku",
-    model = { model = "claude-3-haiku-20240307", max_tokens = 4096, system = system_code_prompt },
+    model = { model = "claude-3-haiku-20240307", max_tokens = 4096 },
+    system_prompt = system_code_prompt,
     provider = "anthropic",
   },
 }
