4e2a2dade0865e66b3744c49dfd42e25dc006b6a
Frank Roeder <fr.coding@proton.me>
2024-07-17 11:03:18 +0200

    (logger): improve logging
    - write debug and error output to file

--- lua/parrot/logger.lua
+++ lua/parrot/logger.lua
@@ -1,40 +1,40 @@
 local M = {
   _plugin_name = "parrot.nvim",
+  _logfile = "/tmp/parrot.nvim.log",
 }
 
----@param msg string # message to log
----@param kind string # hl group to use for logging
----@param history boolean # whether to add the message to history
-M._log = function(msg, kind, history)
-  vim.schedule(function()
-    vim.api.nvim_echo({
-      { M._plugin_name .. ": " .. msg .. "\n", kind },
-    }, history, {})
-  end)
+local function write_to_logfile(msg, kind)
+  local logfile = io.open(M._logfile, "a")
+  if logfile then
+    logfile:write(string.format("[%s] %s: [%s] %s\n", os.date("%Y-%m-%d %H:%M:%S"), M._plugin_name, kind, msg))
+    logfile:close()
+  end
 end
 
--- nicer error messages using nvim_echo
----@param msg string # error message
-M.error = function(msg)
-  M._log(msg, "ErrorMsg", true)
+local function log(msg, kind)
+  if kind == "ErrorMsg" or kind == "Debug" then
+    write_to_logfile(msg, kind)
+  end
+  if kind ~= "Debug" then
+    print(string.format("%s: [%s] %s", M._plugin_name, kind, msg))
+  end
 end
 
--- nicer warning messages using nvim_echo
----@param msg string # warning message
-M.warning = function(msg)
-  M._log(msg, "WarningMsg", true)
+function M.error(msg)
+  log(msg, "ErrorMsg")
 end
 
--- nicer plain messages using nvim_echo
----@param msg string # plain message
-M.info = function(msg)
-  M._log(msg, "Normal", true)
+function M.warning(msg)
+  log(msg, "WarningMsg")
 end
 
----@param msg string # plain message
-M.debug = function(msg)
+function M.info(msg)
+  log(msg, "Normal")
+end
+
+function M.debug(msg)
   if os.getenv("DEBUG_PARROT") then
-    M._log(msg, "Debug", true)
+    log(msg, "Debug")
   end
 end
 
